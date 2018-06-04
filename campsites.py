c0d77d141e7b7599c4c91b7586716e7447b95d5e
Matthew <lopiofsj@gmail.com>
2018-06-04 15:15:30 -0700

    adding more sites and now flake8 compliant

--- campsites.py
+++ campsites.py
@@ -14,6 +14,8 @@ PARKS = {
     '70926': 'TUOLOMNE MEADOWS',
     '70927': 'NORTH PINES',
     '70928': 'LOWER PINES',
+    '70929': 'HODGDON MEADOW',
+    '70930': 'CRANE FLAT',
     '73635': 'STANISLAUS',
 }
 
@@ -27,7 +29,7 @@ LOCATION_PAYLOAD = {
     'resetAllFilters':    'false',
     'filtersFormSubmitted': 'false',
     'glocIndex':    '0',
-    'googleLocations':  'Yosemite National Park, Yosemite Village, CA 95389, USA|-119.53832940000001|37.8651011||LOCALITY'
+    'googleLocations':  'Yosemite National Park, Yosemite Village, CA 95389, USA|-119.53832940000001|37.8651011||LOCALITY'  # noqa: E501
 }
 
 # Sets the search type to camping
@@ -59,6 +61,7 @@ BASE_URL = "https://www.recreation.gov"
 UNIF_SEARCH = "/unifSearch.do"
 UNIF_RESULTS = "/unifSearchResults.do"
 
+
 def findCampSites(args):
     payload = generatePayload(args['start_date'], args['end_date'])
 
@@ -67,21 +70,25 @@ def findCampSites(args):
     sites = getSiteList(html)
     return sites
 
+
 def getNextDay(date):
     date_object = datetime.strptime(date, "%Y-%m-%d")
     next_day = date_object + timedelta(days=1)
     return datetime.strftime(next_day, "%Y-%m-%d")
+
 
 def formatDate(date):
     date_object = datetime.strptime(date, "%Y-%m-%d")
     date_formatted = datetime.strftime(date_object, "%a %b %d %Y")
     return date_formatted
 
+
 def generatePayload(start, end):
     payload = copy.copy(PAYLOAD)
     payload['arrivalDate'] = formatDate(start)
     payload['departureDate'] = formatDate(end)
     return payload
+
 
 def getSiteList(html):
     sites = html.findAll('div', {"class": "check_avail_panel"})
@@ -90,17 +97,22 @@ def getSiteList(html):
         if site.find('a', {'class': 'book_now'}):
             get_url = site.find('a', {'class': 'book_now'})['href']
             # Strip down to get query parameters
-            get_query = get_url[get_url.find("?") + 1:] if get_url.find("?") >= 0 else get_url
+            if get_url.find("?") >= 0:
+                get_query = get_url[get_url.find("?") + 1:]
+            else:
+                get_query = get_url
             if get_query:
                 get_params = parse_qs(get_query)
                 siteId = get_params['parkId']
                 if siteId and siteId[0] in PARKS:
-                    results.append("%s, Booking Url: %s" % (PARKS[siteId[0]], BASE_URL + get_url))
+                    msg = "%s, Booking Url: %s"
+                    found_url = BASE_URL + get_url
+                    results.append(msg % (PARKS[siteId[0]], found_url))
     return results
+
 
 def sendRequest(payload):
     with requests.Session() as s:
-        
         # Sets session cookie
         s.get(BASE_URL + UNIF_RESULTS, verify=True)
         # Sets location to yosemite
@@ -111,15 +123,23 @@ def sendRequest(payload):
         # Runs search on specified dates
         resp = s.post(BASE_URL + UNIF_SEARCH, payload, verify=True)
         if (resp.status_code != 200):
-            raise Exception("failedRequest","ERROR, %d code received from %s".format(resp.status_code, BASE_URL + SEARCH_PATH))
+            msg = "ERROR, %d code received from %s"
+            failed_url = BASE_URL + UNIF_SEARCH
+            raise Exception("failedRequest", msg.format(resp.status_code,
+                                                        failed_url))
         else:
             return resp.text
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--start_date", required=True, type=str, help="Start date [YYYY-MM-DD]")
-    parser.add_argument("--end_date", type=str, help="End date [YYYY-MM-DD]")
+    parser.add_argument("--start_date",
+                        required=True,
+                        type=str,
+                        help="Start date [YYYY-MM-DD]")
+    parser.add_argument("--end_date",
+                        type=str,
+                        help="End date [YYYY-MM-DD]")
 
     args = parser.parse_args()
     arg_dict = vars(args)
